58529c628342936b06b0b654a58faf091ba07ec0
Amir Mirfallahi <Amir Mirfallahi>
2025-09-12 10:13:54 +0330

    Fixed links of the button

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -7,17 +7,20 @@ import CallToAction from "@/components/CallToAction";
 import { initScrollAnimations } from "@/lib/animations";
 import { useAuth } from "@/context/AuthContext";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function Index() {
   useEffect(() => {
     initScrollAnimations();
   }, []);
   const { isAuthenticated } = useAuth();
+  let navigate = useNavigate();
 
   return (
     <div className="min-h-screen">
       <Button
-        className="sticky top-10 left-5 z-[1000] rounded-lg bg-orange-400"
+        className="sticky top-10 left-5 z-[1000] rounded-lg bg-orange-400 cursor-pointer"
+        onClick={() => navigate(isAuthenticated ? "/dashboard" : "/login")}
         size="lg"
       >
         {isAuthenticated ? "داشبورد" : "ورود/ثبت نام"}
