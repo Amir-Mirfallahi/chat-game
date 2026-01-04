852982ef8c7bed8d191de2f4af2c9e87628ecb2d
Amir Mirfallahi <mirfallahi2009@gmail.com>
2026-01-04 17:10:53 +0330

    Building APK

---
+++ frontend/android/app/src/main/java/com/chatgame/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.chatgame.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chatgame.com',
+  appName: 'CHAT',
+  webDir: 'dist'
+};
+
+export default config;
